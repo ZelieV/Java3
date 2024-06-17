4a968337ae8d83044df913961696c4d7c1421e7a
ZelieV <zelie.vdr@gmail.com>
2024-06-17 14:20:48 +0200

    first commit

---
+++ indianajones.java
@@ -0,0 +1,19 @@
+class Indiana {
+    
+    
+    public static void main(String[] args) {
+
+        int dateRelease = 1981;
+        System.out.println(dateRelease);
+
+String title = "Raiders of the Lost Ark";
+    System.out.println(title);
+boolean seenIt = true;
+    System.out.println(seenIt);
+double rating = 8.2;
+    System.out.println(rating);
+
+       
+    }
+}
+
